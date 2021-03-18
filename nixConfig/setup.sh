acb0c73cf2d1356a4ecd91b7a462adc5ae9fdba9
silverest12 <Silverest12@gmail.com>
2021-03-18 23:35:37 +0100

    Vim fixed

---
+++ nixConfig/setup.sh
@@ -0,0 +1,8 @@
+mkdir -p $HOME/.config/nixpkgs/
+cp -r home/* $HOME/.config/nixpkgs/
+nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+nix-shell '<home-manager>' -A install
+cp home/nixos.png $HOME/Pictures/
+home-manager switch
