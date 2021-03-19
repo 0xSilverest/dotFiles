fddc656f3d161dbc351021bf69fdf4462a51a165
silverest12 <Silverest12@gmail.com>
2021-03-19 22:57:32 +0100

    Please work

---
+++ nixConfig/home/programs/xmonad/default.nix
@@ -0,0 +1,15 @@
+{
+  xsession = {
+    enable = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = hp: [
+        hp.monad-logger
+        hp.taffybar
+        hp.xmonad-contrib
+      ];
+      config = ./config.hs;
+    };
+  };
+}
